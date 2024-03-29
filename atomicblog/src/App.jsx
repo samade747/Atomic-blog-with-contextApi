ef092db5002d181e1ca2b83b83349dd2a40fd7c1
samade747 <samad.e747@gmail.com>
2024-03-30 04:17:45 +0500

    417am 30 3 2024

--- atomicblog/src/App.jsx
+++ atomicblog/src/App.jsx
@@ -2,3 +2,35 @@ import React, { useEffect } from "react";
 import { useStateValue } from "./AppContext";
 import { faker } from "@faker-js/faker";
 
+function createRandomPost(){
+    return {
+        title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+        body: faker.hacker.phrase(),
+    };
+
+};
+
+// Define the App component
+function App() {  
+    // Get the isFakeDark state and dispatch function from the context
+    const [{ isFakeDark}, dispatch] = useStateValue(); 
+    // This line uses the useStateValue hook to get the isFakeDark state and the dispatch function from the context.
+    // The useStateValue hook returns an array with the current state and a dispatch function that can be used to update the state.
+
+    const [posts, setPosts] = React.useState(() => 
+       Array.from({length: 30}, () => createRandomPost())
+       // This line initializes the posts state using the useState hook, setting it to an array of 30 randomly generated posts.
+       
+    );
+
+    
+
+
+
+
+
+
+
+
+}
+
